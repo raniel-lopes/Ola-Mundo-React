c93af18c7877c8a84264734b32bf3db13656a1cc
Raniel Lopes <raniel.ldms@gmail.com>
2024-12-24 21:27:52 -0300

    finalizando a página de post e prevenindo erros para encontrar o id

--- src/Paginas/Post/index.jsx
+++ src/Paginas/Post/index.jsx
@@ -1,11 +1,31 @@
+import './Post.css'
 import { useParams } from "react-router"
+import posts from "json/posts.json"
+import PostModelo from 'Componentes/PostModelo'
+import ReactMarkdown from 'react-markdown'
+
 
 export default function Post() {
     const parametros = useParams();
 
-    console.log(parametros)
+    const post = posts.find((post) => {
+        return post.id === Number(parametros.id);
+    })
+
+    if (!post) {
+        return <h1>Post não encontrado</h1>
+    }
 
     return (
-        <h1>Post {parametros.id}</h1>
+        <PostModelo
+            fotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
+        >
+            <div className="post-markdown-container">
+                <ReactMarkdown>
+                    {post.texto}
+                </ReactMarkdown>
+            </div>
+        </PostModelo>
     )
 }
